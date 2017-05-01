e5db73f51298020e91ec7acacc485bb490599692
Stephanie Ramsay <stephanie.ramsay@mymona.uwi.edu>
2017-05-01 05:21:27 +0000

    third commit

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ import smtplib
 app = Flask(__name__)
 
 
-#app.config['SQLALCHEMY_DATABASE_URI']='postgresql://project01:wishlist@localhost/project01'
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://vjmyqsccqutxaw:6a151746ead95f3a8f14136851adccfc248f4355e722c2a8257cb8784840a062@ec2-54-235-72-121.compute-1.amazonaws.com:5432/d1u4ulsb830vdc'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://project01:wishlist@localhost/project01'
+#app.config['SQLALCHEMY_DATABASE_URI']='postgresql://vjmyqsccqutxaw:6a151746ead95f3a8f14136851adccfc248f4355e722c2a8257cb8784840a062@ec2-54-235-72-121.compute-1.amazonaws.com:5432/d1u4ulsb830vdc'
 app.config['SECRET_KEY']='shh..itsasecret'
 app.config['UPLOAD_FOLDER'] = "./app/static/UPLOADS"
 db = SQLAlchemy(app)
